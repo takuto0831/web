999190a4cf6f16db244bb9fac5fce657fa7a951a
Takuto Kotsubo <takut0.mirage@gmail.com>
2020-05-09 19:52:20 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function changeLang(lang) {
+    if (lang == 'jp') {
+      $('.en').each(function () {
+        $(this).css('display', 'none');
+        $(this).prev($(this).prop('tagName')).fadeIn();
+        langEng = false;
+      });
+    } else if (lang == 'en') {
+      $('.en').each(function () {
+        $(this).prev($(this).prop('tagName')).css('display', 'none');
+        $(this).fadeIn();
+        langEng = true;
+      });
+    }
+  }
