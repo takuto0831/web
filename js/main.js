4d7c8d4f9c266f42671d723f3738dea47e49185d
takuto0831 <takut0.mirage@gmail.com>
2020-05-30 01:07:15 +0900

    add

--- js/main.js
+++ js/main.js
@@ -13,4 +13,32 @@ function changeLang(lang) {
       });
     }
   }
+function post_text(){
+    // 値を取得
+    const new_text = document.super_search.textarea.value; 
+    const old_text = document.getElementById("search_text").textContent;
+    document.super_search.textarea.value = ""; // delete textarea
+    // need space or not (and, or: need, remove: not need)
+    if(old_text.slice(-1) == '-'){
+        document.getElementById("search_text").textContent = old_text + new_text;
+    }else {
+        document.getElementById("search_text").textContent = old_text + ' ' + new_text;
+    }
+}
+function delete_text(){
+    document.getElementById("search_text").textContent = "";
+}
+function add_text(){
+    // get option
+    conditional_text = document.super_search.pullMenu.value ;
+    const old_text = document.getElementById("search_text").textContent;
+    // combine text
+    document.getElementById("search_text").textContent = old_text + ' ' + conditional_text;
+    // reset selected value
+    document.getElementById("pullMenu").value = ""
+}
 
+function search_text() {
+  const text = document.getElementById("search_text").textContent;
+  location.href='https://www.google.com/search?q='+text;
+}
